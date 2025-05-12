a6125490f70321d40c5f01f2cbf312ad7079fd14
Alex <alexgllamosas@gmail.com>
2025-05-12 15:14:47 -0300

    Products new Schema

--- models/product.js
+++ models/product.js
@@ -4,13 +4,14 @@ import { stringify } from "uuid";
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
-  category: { type: String, required: true },
+  category: { type: [String]},
   imageURL: { type: String, required: true },
   variants: [
     {
       abreviation: String,
-      size: String,
+      variantName: String,
       price: Number,
+      barcode:String
     },
   ],
   createdAt: { type: Date, default: Date.now }
